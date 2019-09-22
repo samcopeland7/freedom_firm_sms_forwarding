aeda54cba3aadc89d693e79d69873692efe24572
Sam Copeland <linkherox@gmail.com>
2019-09-22 14:43:40 -0400

    Set up users with an admin option for message forwarding

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190903183744) do
+ActiveRecord::Schema.define(version: 20190920015021) do
 
   create_table "subscribers", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone_number"
     t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
