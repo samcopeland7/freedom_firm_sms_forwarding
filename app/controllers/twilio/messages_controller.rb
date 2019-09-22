c5e2ffb8f4d1664ea4c1b739e4998b3d9d835910
Sam Copeland <linkherox@gmail.com>
2019-09-22 15:38:01 -0400

    Finish basic functionality for handling text messages

--- app/controllers/twilio/messages_controller.rb
+++ app/controllers/twilio/messages_controller.rb
@@ -2,20 +2,46 @@ class Twilio::MessagesController < ApplicationController
   include Webhookable
   skip_before_action :verify_authenticity_token
 
-  def welcome
-    unless (subscriber = Subscriber.find_by(phone_number: params[:From]))
-      subscriber = Subscriber.create!(subscriber_params)
-    end
+  def handle
+    if (user = User.find_by(phone_number: params[:From]))
+      return unless user.admin?
+      @client = Twilio::REST::Client.new ENV.fetch('TWILIO_ACCOUNT_SID'),
+                                         ENV.fetch('TWILIO_AUTH_TOKEN')
+      Subscriber.all.each do |sub|
+        @client.messages.create(
+          from: ENV.fetch('TWILIO_PHONE_NUMBER'),
+          to: sub.phone_number,
+          body: params[:Body]
+        )
+      end
+    else
+      unless (@subscriber = Subscriber.find_by(phone_number: params[:From]))
+        @subscriber = Subscriber.create!(subscriber_params)
+        @new_subscriber = true
+      end
 
-    message = Twilio::TwiML::MessagingResponse.new do |r|
-      r.message(
-        body: 'You will now receive prayer updates for Freedom Firm. Reply HELP for help. Reply STOP to no longer receive messages.',
-        from: ENV.fetch('TWILIO_PHONE_NUMBER'),
-        to: subscriber.phone_number
-      )
-    end
+      if params[:Body].match?(/stop/i)
+        response_body = 'You have unsubscribed from Freedom Firm updates. You will no longer ' \
+          'receive future prayer updates.'
+        @subscriber.destroy!
+      elsif @new_subscriber
+        response_body = 'You will now receive prayer updates for Freedom Firm. Reply STOP ' \
+          'to no longer receive messages.' 
+      else
+        response_body = 'You are already subscribed to updates from Freedom Firm. Reply STOP '\
+          'to no longer receive messages.'
+      end
 
-    render_twiml message
+      message = Twilio::TwiML::MessagingResponse.new do |r|
+        r.message(
+          body: response_body,
+          from: ENV.fetch('TWILIO_PHONE_NUMBER'),
+          to: @subscriber.phone_number
+        )
+      end
+
+      render_twiml message
+    end
   end
 
   def help
@@ -26,9 +52,11 @@ class Twilio::MessagesController < ApplicationController
 
   def subscribe
     Subscriber.create!(subscriber_params)
+    render status: 200
   end
 
   def forward
+    render plain: params
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :twilio do
-    post '/', to: 'switchboard#reroute'
-    post 'help', to: 'messages#help'
-    post 'welcome', to: 'messages#welcome'
-    post 'stop', to: 'messages#stop'
-    post 'forward', to: 'messages#forward'
+    post '/', to: 'messages#handle'
+    # post 'help', to: 'messages#help'
+    # post 'stop', to: 'messages#stop'
+    # post 'forward', to: 'messages#forward'
+    # post 'subscribe', to: 'messages#subscribe'
   end
 
   resources :subscribers
